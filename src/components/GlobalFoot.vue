b7e00f645d5e7d342ffd3523631da51bb9b05c30
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2024-04-23 22:17:50 -0500

    built and parsed

--- src/components/GlobalFoot.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <footer>
-        <p>Davy Jones</p>
-        <a href="https://github.com/davyjonesdesign/davyjones" target="_blank" class="github-repo">Github Repository</a>
-        <p>© 2024</p>
-    </footer>
-</template>
